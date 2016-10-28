66213615f28957869b8d419490bb857843fba4e3
jeonghun Ji <jj.jeonghun.ji@gmail.com>
2016-10-28 12:46:39 +0900

    .

--- src/bitcamp/java89/ems/LecController.java
+++ src/bitcamp/java89/ems/LecController.java
@@ -2,43 +2,54 @@ package bitcamp.java89.ems;
 import java.util.Scanner;
 
 public class LecController {
-  Lecture[] lecss = new Lecture[100];
-  int length = 0;
-  Scanner keyScan;
-
-
-  public LecController (Scanner keyScan) {
+  private Lecture[] lecss = new Lecture[100];
+  private int length = 0;
+  private Scanner keyScan;
+  public LecController(Scanner keyScan) {
     this.keyScan = keyScan;
   }
 
-  public void doAdd() {
+  public void service() {
+    while (true) {
+      System.out.print("강의관리> ");
+      String command = keyScan.nextLine().toLowerCase();
+
+
+      switch (command) {
+        case "add": this.doAdd(); break;
+        case "list": this.doList(); break;
+        case "view": this.doView(); break;
+        case "delete": this.doDelete(); break;
+        case "main":
+          break loop;
+        default:
+          System.out.println("지원하지 않는 명령어 입니다.");
+      }
+    }
+  }
+
+
+  private void doAdd() {
     while ( length < this.lecss.length) {
       Lecture lec = new Lecture();
 
       System.out.print("강의이름 ?");
       lec.name = this.keyScan.nextLine();
-
       System.out.print("선생님성함 ?");
       lec.teacher = this.keyScan.nextLine();
-
       System.out.print("교실번호?(예 : 233)");
       lec.room = Integer.parseInt(this.keyScan.nextLine());
-
       System.out.print("강의주제 ?");
       lec.subject = this.keyScan.nextLine();
-
       System.out.print("수업시간 ?");
       lec.time = this.keyScan.nextLine();
-
       System.out.print("국가지원여부 (y/n)");
       lec.support = (this.keyScan.nextLine().equals("y")) ? true : false ;
-
       System.out.print("강의료(예:18000)");
       lec.money = Integer.parseInt(this.keyScan.nextLine());
-
       System.out.print("강의번호");
       lec.noname = Integer.parseInt(this.keyScan.nextLine());
-      this.lecss[length++] = lec;
+      lecss[length++] = lec;
 
       System.out.print("계속 입력하시겠습니까(y/n)?");
       if (!keyScan.nextLine().equals("y"))
@@ -46,7 +57,7 @@ public class LecController {
 
     }
   }
-  public void doList() {
+  private void doList() {
 
     for (int i = 0; i < this.length;  i++) {
     Lecture lec = this.lecss[i];
@@ -61,7 +72,7 @@ public class LecController {
     }
   }
 
-  public void doView() {
+  private void doView() {
     System.out.print("조회할 학생의 아이디는? ");
     String name = this.keyScan.nextLine().toLowerCase();
     for (int i = 0; i < this.length; i ++) {
@@ -77,5 +88,20 @@ public class LecController {
         break;
       }
     }
+  }
+  private void doDelete() {
+    System.out.print("삭제할 학생의 아이디는? ");
+    String name = this.keyScan.nextLine().toLowerCase();
+    for (int i = 0; i < this.length; i ++) {
+      if (this.lecss[i].name.toLowerCase().equals(name)) {
+        for(int x = i + 1; x < length; x++, i ++) {
+          lecss[i] = lecss[x];
+        }
+        lecss[--length] = null;
+        System.out.printf("%s 학생 정보를 삭제 하였습니다.\n", name);
+        return;
+      }
+    }
+    System.out.printf("%s 학생이 없습니다.\n", name);
   }
 }
---
+++ src/bitcamp/java89/ems/Lecture.java
@@ -0,0 +1,32 @@
+package step09.ex1;
+
+public class Lecture {
+  //인스턴스 변수 만들기
+  String name; //강의 이름
+  String teacher; // 선생님이름
+  int room; // 교실 번호
+  String subject; // 강의 주제
+  String time; // 수업 시간
+  boolean support; // 지원여부
+  int money; // 강의료
+  int noname;// 강의 번호
+
+
+  public Lecture() {}
+
+  public Lecture(String name, int noname, String subject) {
+    this.name = name;
+    this.noname = noname;
+    this.subject = subject;
+  }
+
+
+
+
+
+
+
+
+
+
+}
